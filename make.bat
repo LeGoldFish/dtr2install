89c3acb660cbfe8ff67c0bf066a2aa7cfc12159c
legoldfish <mrseskelin@yahoo.com>
2015-01-20 09:24:59 -0500

    Added make.bat

---
+++ make.bat
@@ -0,0 +1,7 @@
+@ECHO off
+md "%USERPROFILE%\Desktop\dtr2install"
+md "%USERPROFILE%\Desktop\dtr2install\skins"
+md "%USERPROFILE%\Desktop\dtr2install\cars"
+md "%USERPROFILE%\Desktop\dtr2install\tracks"
+ECHO Finished, enjoy using.
+pause
