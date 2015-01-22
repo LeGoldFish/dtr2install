0306a4cb19ca21e5baf69868b124a1a7dd68ad4a
legoldfish <mrseskelin@yahoo.com>
2015-01-22 15:05:18 -0500

    Cleaned up aome stuff.

--- make.bat
+++
@@ -1,7 +0,0 @@
-@ECHO off
-md "%USERPROFILE%\Desktop\dtr2install"
-md "%USERPROFILE%\Desktop\dtr2install\skins"
-md "%USERPROFILE%\Desktop\dtr2install\cars"
-md "%USERPROFILE%\Desktop\dtr2install\tracks"
-ECHO Finished, enjoy using.
-pause
